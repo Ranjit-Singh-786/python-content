d5c0e61022b9b2affb2052b8e16c3401b4daf015
Ranjit-Singh-786 <jiradhey402@gmail.com>
2021-07-07 22:29:50 +0530

    table progrm upload

---
+++ table.py
@@ -0,0 +1,12 @@
+numbr = int(input("enter your number please ! :- "))
+# i = 1
+# while i<=10:
+# 	print(numbr,'*',i,'=',numbr*i)
+# 	i = i+1
+
+
+
+for i in range(1,11):
+	print(numbr,"*",i,"=" ,numbr*i)
+
+
